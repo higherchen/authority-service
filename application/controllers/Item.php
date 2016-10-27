f60062ae4d631e9ace6e12ad1695516fced99dde
higher <chenzhenhai@bilibili.com>
2016-10-27 18:01:16 +0800

    add 2 controllers

---
+++ application/controllers/Item.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+* ItemController
+*
+* 权限点/权限分类/角色组/权限组管理
+*
+* @package controllers
+*/
+
+class ItemController extends Yaf_Controller_Abstract
+{
+    public function get()
+    {
+    }
+
+    public function add()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function remove()
+    {
+    }
+}
---
+++ application/controllers/Rule.php
@@ -0,0 +1,20 @@
+<?php
+
+class RuleController extends Yaf_Controller_Abstract
+{
+    public function get()
+    {
+    }
+
+    public function add()
+    {
+    }
+
+    public function update()
+    {
+    }
+
+    public function remove()
+    {
+    }
+}
