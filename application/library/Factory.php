da08f4a9b0666bb4345d86bf6a294fa0b9c24ae8
higher <chenzhenhai@bilibili.com>
2016-11-04 12:23:50 +0800

    fix factory a little

--- application/library/Factory.php
+++ application/library/Factory.php
@@ -5,19 +5,21 @@ class Factory
     public static $rules = [];
     public static $users = [];
 
-    public static function getRule($rule_name)
+    public static function getRule($rule)
     {
+        $rule_name = is_array($rule) ? $rule['name'] : $rule;
         if (!isset(static::$rules[$rule_name])) {
-            static::$rules[$rule_name] = new Authority_Rule($rule_name);
+            static::$rules[$rule_name] = new Authority_Rule($rule);
         }
 
         return static::$rules[$rule_name];
     }
 
-    public static function getUser($uid)
+    public static function getUser($user)
     {
+        $uid = is_array($user) ? $user['id'] : $user;
         if (!isset(static::$users[$uid])) {
-            static::$users[$uid] = new Authority_User($uid);
+            static::$users[$uid] = new Authority_User($user);
         }
 
         return static::$users[$uid];
